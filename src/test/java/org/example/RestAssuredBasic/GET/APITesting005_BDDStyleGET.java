0966071d0b3045ee57ed03e51dfab218fe6d71f0
abhishek <abhishek@jcsoftwaresolution.com>
2024-12-17 13:34:55 +0530

    6th dec

--- src/test/java/org/example/RestAssuredBasic/GET/APITesting005_BDDStyleGET.java
+++ src/test/java/org/example/RestAssuredBasic/GET/APITesting005_BDDStyleGET.java
@@ -10,15 +10,15 @@ public class APITesting005_BDDStyleGET {
         String pin_code =  "388620";
     RestAssured
             .given()
-            .baseUri("https://api.zippopotam.us")
-            .basePath("/IN/"+pin_code)
+              .baseUri("https://api.zippopotam.us")
+              .basePath("/IN/"+pin_code)
             .when()
-            .log()
-            .all()
-            .get()
+              .log()
+              .all()
+              .get()
             .then()
-            .log().all()
-            .statusCode(200);
+              .log().all()
+              .statusCode(200);
 
 
     }
