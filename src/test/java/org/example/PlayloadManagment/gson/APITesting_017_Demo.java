9f0d8af7bf4529c33d840052b4c332847b5e3169
abhishek <abhishek@jcsoftwaresolution.com>
2024-12-17 15:46:48 +0530

    6th dec

---
+++ src/test/java/org/example/PlayloadManagment/gson/APITesting_017_Demo.java
@@ -0,0 +1,12 @@
+package org.example.PlayloadManagment.gson;
+
+
+public class APITesting_017_Demo {
+    public static void main(String[] args) {
+
+        person p1 = new person();
+        p1.setName("Mansi");
+        System.out.println(p1.getName());
+    }
+
+}
---
+++ src/test/java/org/example/PlayloadManagment/gson/person.java
@@ -0,0 +1,27 @@
+package org.example.PlayloadManagment.gson;
+
+public class person {
+    private String name;
+    private Integer age;
+
+    public person()
+    {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
